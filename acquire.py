a651784ac1ee9583009fa739febb39abe49a8e7e
Adam Harris <afharris87@gmail.com>
2023-05-11 12:03:08 -0500

    working on acquire

--- acquire.py
+++ acquire.py
@@ -54,27 +54,27 @@ def get_blog_content(url, headers):
     # get the content from this blog
     response = requests.get(url, headers=headers)
     # check if we have correct response and if there is content in our response
-    if response.status_code == 200:
-        # convert our received content into soup object
-        soup = BeautifulSoup(response.content, 'html.parser')
-        # have content, will travel
-        if soup:
-            # gather the title from the <h2> tag
-            title = soup.find('h1', class_='entry-title').text
-            # get the blog content text, strip extra chars
-            content = soup.find('div', class_='entry-content').text.strip()
-            # create a regex function to search for the title, while ignoring case
-            compiled = re.compile(re.escape(title), re.IGNORECASE)
-            # remove the title from the content, while ignoring case
-            content = compiled.sub('', content)
-        # if there is no soup
-        else:
-            # display an error message
-            print('page has no content')
-    # if there is a page error
-    else:
-        # display an error message
+    if response.status_code != 200: # if there is a page error
         print('page url has returned an error')
+        return
+        
+    # convert our received content into soup object
+    soup = BeautifulSoup(response.content, 'html.parser')
+    if not soup: # if there is no soup
+        # display an error message
+        print('page has no content')
+        return
+    
+    # have soup, will travel
+    # gather the title from the <h2> tag
+    title = soup.find('h1', class_='entry-title').text
+    # get the blog content text, strip extra chars
+    content = soup.find('div', class_='entry-content').text.strip()
+    # create a regex function to search for the title, while ignoring case
+    compiled = re.compile(re.escape(title), re.IGNORECASE)
+    # remove the title from the content, while ignoring case
+    content = compiled.sub('', content)
+    
     # return the scraped title and content for this blog
     return title, content
 
@@ -112,59 +112,113 @@ def get_blog_articles(fresh=False):
     # get a list of blog page urls
     link_list = get_blog_links(url, headers)
     # create an empty dictionary to store the results
-    content_dict = {}
+    content_dict = []
     # display a status message
     print('Gathering blog content')
     # cycle through all the gathered blog links
-    for i, blog in enumerate(link_list):
+    for blog in link_list:
         # gather the title and content for each page
         title, content = get_blog_content(blog, headers)
         # add a record to the dictionary with the title, url and content for the blog
-        content_dict[i] = {'title':title, 'url':blog, 'content':content}
+        article = {'title':title, 'url':blog, 'content':content}
+        content_dict.append(article)
         
     # cache the dictionary data to a csv
-    pd.DataFrame(content_dict).T.to_csv(filename)
+    pd.DataFrame(content_dict).to_csv(filename)
     # return the dictionary of blogs
     return content_dict
 
 #---------------------------------------------
 
-def get_news_articles():
+def get_news_articles(fresh=False):
+    '''
+    This will gather news articles from https://inshorts.com in the categories:
+    business, sports, technology and entertainment. it will return the news articles
+    as a dictionary
+    '''
+    filename = 'inshorts_news.csv'
+    # check if we need fresh data
+    if not fresh:
+        # if we don't need fresh data, open the cached data 
+        # check if data csv exists
+        if os.path.exists(filename):
+            # print status message
+            print(f'Opening news data from local file {filename}')
+            # open the local csv data
+            inshorts = pd.read_csv(filename, index_col=0)
+            # return the dataset (ending the function)
+            return inshorts
+        # if the file is not found
+        else:
+            # print status message
+            print(f'File {filename} not found in local directory')
+    # download the data
+    # print status message
+    print('downloading news data from inshorts.com')
     # assign the url
     url = 'https://inshorts.com/en/read/'
     # request the data from the url
     response = requests.get(url)
     # check if good status code
+    
     if response.status_code != 200:
-        print('error code from request')
-    else:
+        # print error status message
+        print('error code from http request')
+        return
+    # if there is no error with the request
+    # turn data into soup
+    soup = BeautifulSoup(response.content, 'html.parser')
+    # if we dont have soup, give an error
+    if not soup:
+        print('no content found on page')
+        return
+    # have soup, will travel
+    
+    # <li> is the category list
+    # get rid of the all news category
+    categories = [li.text.lower() for li in soup.select('li')][1:]
+    # the india category link is actually 'national'
+    categories[0] = 'national'
+    # create an empty list to store data
+    inshorts = []
+    # cycle through every category
+    for category in categories:
+        # set the url
+        url = 'https://inshorts.com/en/read/' + category
+        # get the data
+        response = requests.get(url)
+        
+        if response.status_code != 200:
+            # print error status message
+            print('error code from http request')
+            return
+        # if there is no error with the request
         # turn data into soup
         soup = BeautifulSoup(response.content, 'html.parser')
         # if we dont have soup, give an error
         if not soup:
             print('no content found on page')
-        else:
-            # get rid of the all news category
-            categories = [li.text.lower() for li in soup.select('li')][1:]
-            # the india category link is actually 'national'
-            categories[0] = 'national'
-
-            inshorts = []
-            for category in categories:
-                url = 'https://inshorts.com/en/read/' + category
-                response = requests.get(url)
-                soup = BeautifulSoup(response.content, 'html.parser')
-
-                titles = [span.text for span in soup.find_all('span', 
-                                                              itemprop='headline')]
-                content = [div.text for div in soup.find_all('div', 
-                                                             itemprop='articleBody')]
-
-                for i in range(len(titles)):
-                    article = {
-                        'title': titles[i],
-                        'content': content[i],
-                        'category': category
-                    }
-                    inshorts.append(article)
+            return
+            
+        # have soup, will travel
+        # get the article titles
+        titles = [span.text for span in soup.find_all('span', 
+                                                  itemprop='headline')]
+        # get the article content
+        content = [div.text for div in soup.find_all('div', 
+                                                 itemprop='articleBody')]
+        # cycle through all the titles
+        for i in range(len(titles)):
+            # create a dictionary containing the current article
+            article = {
+                'title': titles[i],
+                'content': content[i],
+                'category': category
+            }
+            # add each article to our list of articles
+            inshorts.append(article)
+    
+    # cache the dictionary data to a csv
+    pd.DataFrame(inshorts).to_csv(filename)
+    # return the news articles
     return inshorts
